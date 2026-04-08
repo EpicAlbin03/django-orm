cc6c2e36756b9e2dc021281d6e2854b018cab3f4
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-04-08 13:46:45 +0200

    edit student

--- studentplatform/forms.py
+++ studentplatform/forms.py
@@ -1,9 +1,19 @@
-from django import forms
+from typing import cast
 
-from .models import Student
+from django import forms
+from django.db import models
+
+from .models import Course, Student
+
+
+class CourseChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj: models.Model) -> str:
+        return cast(Course, obj).name
 
 
 class StudentForm(forms.ModelForm):
+    course = CourseChoiceField(queryset=Course.objects.all(), empty_label=None)
+
     class Meta:
         model = Student
         fields = ["name", "email", "date_of_birth", "grade", "is_active", "course"]
